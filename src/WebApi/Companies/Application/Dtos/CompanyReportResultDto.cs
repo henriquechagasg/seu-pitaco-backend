c6b9104154c4915d2d970d125c8f20afcc735497
Henrique Chagas <henriquechagasg@gmail.com>
2025-12-03 18:25:57 -0300

    feat: calculate company nps result

---
+++ src/WebApi/Companies/Application/Dtos/CompanyReportResultDto.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace WebApi.Companies.Application.Dtos;
+
+public class CompanyReportResultDto
+{
+    public int Count { get; set; }
+    public float Nps { get; set; }
+}
--- src/WebApi/Companies/Application/Queries/CompanyReportQuery.cs
+++ src/WebApi/Companies/Application/Queries/CompanyReportQuery.cs
@@ -1,14 +1,18 @@
 using Amazon.DynamoDBv2.DataModel;
+using WebApi.Companies.Application.Dtos;
 using WebApi.Surveys.Domain.Entities;
 
 namespace WebApi.Companies.Application.Queries;
 
 public class CompanyReportQuery(IDynamoDBContext _context)
 {
-    public async Task<List<Survey>> Handle()
+    public async Task<CompanyReportResultDto> Handle()
     {
         var surveys = await ScanSurveysAsync();
-        return surveys;
+
+        var Nps = CalculateNPS(surveys);
+
+        return new CompanyReportResultDto { Count = surveys.Count, Nps = Nps };
     }
 
     private async Task<List<Survey>> ScanSurveysAsync()
@@ -18,5 +22,23 @@ public class CompanyReportQuery(IDynamoDBContext _context)
         );
         var searchResponse = await search.GetRemainingAsync();
         return searchResponse;
+    }
+
+    public float CalculateNPS(List<Survey> surveys)
+    {
+        var validSurveys = surveys.Where(s => s.NpsScore.HasValue).ToList();
+
+        int totalResponses = validSurveys.Count;
+
+        if (totalResponses == 0)
+            return 0f;
+
+        int promoters = validSurveys.Count(s => s.NpsScore >= 9);
+
+        int detractors = validSurveys.Count(s => s.NpsScore <= 6);
+
+        float nps = (float)(promoters - detractors) / totalResponses * 100f;
+
+        return nps;
     }
 }
